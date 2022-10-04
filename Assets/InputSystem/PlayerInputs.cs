1384333a97f9503a8c393e1be7ef7f6b84ffd4ac
Takeo Watanabe <takeowatanabe3@gmail.com>
2022-10-05 01:17:03 +0200

    Update PlayerInputs.cs

--- Assets/InputSystem/PlayerInputs.cs
+++ Assets/InputSystem/PlayerInputs.cs
@@ -3,111 +3,107 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 #endif
 
-namespace StarterAssets
+public class PlayerInputs : MonoBehaviour
 {
-	public class PlayerInputs : MonoBehaviour
+	[Header("Character Input Values")]
+	public Vector2 move;
+	public Vector2 look;
+	public bool jump;
+	public bool sprint;
+	public bool changeVision;
+	public bool crouch;
+	public bool interact;
+
+	[Header("Movement Settings")]
+	public bool analogMovement;
+
+	[Header("Mouse Cursor Settings")]
+	public bool cursorLocked = true;
+	public bool cursorInputForLook = true;
+
+	#if ENABLE_INPUT_SYSTEM
+	public void OnMove(InputValue value)
 	{
-		[Header("Character Input Values")]
-		public Vector2 move;
-		public Vector2 look;
-		public bool jump;
-		public bool sprint;
-		public bool changeVision;
-		public bool crouch;
-		public bool interact;
+		MoveInput(value.Get<Vector2>());
+	}
 
-		[Header("Movement Settings")]
-		public bool analogMovement;
-
-		[Header("Mouse Cursor Settings")]
-		public bool cursorLocked = true;
-		public bool cursorInputForLook = true;
-
-#if ENABLE_INPUT_SYSTEM
-		public void OnMove(InputValue value)
+	public void OnLook(InputValue value)
+	{
+		if(cursorInputForLook)
 		{
-			MoveInput(value.Get<Vector2>());
-		}
-
-		public void OnLook(InputValue value)
-		{
-			if(cursorInputForLook)
-			{
-				LookInput(value.Get<Vector2>());
-			}
-		}
-
-		public void OnJump(InputValue value)
-		{
-			JumpInput(value.isPressed);
-		}
-
-		public void OnSprint(InputValue value)
-		{
-			SprintInput(value.isPressed);
-		}
-
-		public void OnChangeVision(InputValue value)
-		{
-			ChangeVisionInput(value.isPressed);
-		}
-		
-		public void OnCrouch(InputValue value)
-		{
-			CrouchInput(value.isPressed);
-		}
-
-		public void OnInteract(InputValue value)
-		{
-			InteractInput(value.isPressed);
-		}
-#endif
-
-
-		public void MoveInput(Vector2 newMoveDirection)
-		{
-			move = newMoveDirection;
-		} 
-
-		public void LookInput(Vector2 newLookDirection)
-		{
-			look = newLookDirection;
-		}
-
-		public void JumpInput(bool newJumpState)
-		{
-			jump = newJumpState;
-		}
-
-		public void SprintInput(bool newSprintState)
-		{
-			sprint = newSprintState;
-		}
-
-		public void ChangeVisionInput(bool newChangeVisionState)
-		{
-			changeVision = newChangeVisionState;
-		}
-
-		public void CrouchInput(bool newCrouchState)
-		{
-			crouch = newCrouchState;
-		}
-
-		public void InteractInput(bool newInteractState)
-		{
-			interact = newInteractState;
-		}
-
-		private void OnApplicationFocus(bool hasFocus)
-		{
-			SetCursorState(cursorLocked);
-		}
-
-		private void SetCursorState(bool newState)
-		{
-			Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+			LookInput(value.Get<Vector2>());
 		}
 	}
+
+	public void OnJump(InputValue value)
+	{
+		JumpInput(value.isPressed);
+	}
+
+	public void OnSprint(InputValue value)
+	{
+		SprintInput(value.isPressed);
+	}
+
+	public void OnChangeVision(InputValue value)
+	{
+		ChangeVisionInput(value.isPressed);
+	}
 	
+	public void OnCrouch(InputValue value)
+	{
+		CrouchInput(value.isPressed);
+	}
+
+	public void OnInteract(InputValue value)
+	{
+		InteractInput(value.isPressed);
+	}
+	#endif
+
+
+	public void MoveInput(Vector2 newMoveDirection)
+	{
+		move = newMoveDirection;
+	} 
+
+	public void LookInput(Vector2 newLookDirection)
+	{
+		look = newLookDirection;
+	}
+
+	public void JumpInput(bool newJumpState)
+	{
+		jump = newJumpState;
+	}
+
+	public void SprintInput(bool newSprintState)
+	{
+		sprint = newSprintState;
+	}
+
+	public void ChangeVisionInput(bool newChangeVisionState)
+	{
+		changeVision = newChangeVisionState;
+	}
+
+	public void CrouchInput(bool newCrouchState)
+	{
+		crouch = newCrouchState;
+	}
+
+	public void InteractInput(bool newInteractState)
+	{
+		interact = newInteractState;
+	}
+
+	private void OnApplicationFocus(bool hasFocus)
+	{
+		SetCursorState(cursorLocked);
+	}
+
+	private void SetCursorState(bool newState)
+	{
+		Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+	}
 }
