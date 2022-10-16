e6bb670519b9c37b9a0a396108ae2e48d35ac194
Takeo Watanabe <takeowatanabe3@gmail.com>
2022-10-16 23:59:46 +0200

    taregets animations fix

--- Assets/Shooting-Target-Set/Scrips/ShootingRangeManager.cs
+++ Assets/Shooting-Target-Set/Scrips/ShootingRangeManager.cs
@@ -5,26 +5,21 @@ using UnityEngine;
 
 public class ShootingRangeManager : MonoBehaviour
 {
-    private ShootingRangeTarget[] targets;
+    [SerializeField] private ShootingRangeTarget[] targets;
     [SerializeField] private bool reset;
     // Start is called before the first frame update
     void Start()
     {
-        targets = FindObjectsOfType<ShootingRangeTarget>();
+        
     }
 
     private void Update()
     {
-        if(!reset) return;
-        Reset();
-        reset = false;
+        
     }
 
     private void Reset()
     {
-        foreach (var x in targets)
-        {
-            StartCoroutine(x.Reset());
-        }
+        
     }
 }
--- Assets/Shooting-Target-Set/Scrips/ShootingRangeTarget.cs
+++ Assets/Shooting-Target-Set/Scrips/ShootingRangeTarget.cs
@@ -4,50 +4,27 @@ using UnityEngine;
 
 public class ShootingRangeTarget : Hittable
 {
-    [SerializeField] public bool hit = false;
-    [SerializeField] private Vector3 hitAngle;
-    [SerializeField] private float rotateDuration = 1f;
-    private Quaternion defaultAngle;
+    [SerializeField] private Animator _animator;
 
     private void Start()
     {
-        defaultAngle = transform.localRotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!hit) return;
-        StartCoroutine(Hitted());
-        hit = false;
+        
     }
 
     public override void OnHit()
     {
-        hit = true;
     }
     
-    private IEnumerator Hitted()
+    private void Hitted()
     {
-        float elapsedTime = 0f;
-
-        while (elapsedTime < rotateDuration)
-        {
-            elapsedTime += Time.deltaTime;
-            transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(hitAngle), elapsedTime/rotateDuration);
-            yield return null;
-        }
     }
     
-    public IEnumerator Reset()
+    public void Reset()
     {
-        float elapsedTime = 0f;
-
-        while (elapsedTime < rotateDuration)
-        {
-            elapsedTime += Time.deltaTime;
-            transform.localRotation = Quaternion.Lerp(transform.localRotation, defaultAngle, elapsedTime/rotateDuration);
-            yield return null;
-        }
     }
 }
