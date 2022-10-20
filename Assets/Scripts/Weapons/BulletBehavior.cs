0fc86ae17878392778760f942237b671712c322c
Takeo Watanabe <takeowatanabe3@gmail.com>
2022-10-21 01:00:54 +0200

    coses

--- Assets/Scripts/Weapons/BulletBehavior.cs
+++ Assets/Scripts/Weapons/BulletBehavior.cs
@@ -22,7 +22,8 @@ public class BulletBehavior : MonoBehaviour
         if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hitInfo, distance)){
             hitInfo.transform.GetComponent<Hittable>()?.OnHit();
             // hitInfo.transform.GetComponent<IDamageable>()?.TakeDamage(gunData.damage);
-            var d = Instantiate(decal, hitInfo.point + (hitInfo.normal * 0.01f), Quaternion.FromToRotation(Vector3.up, hitInfo.normal));
+            //var d = Instantiate(decal, hitInfo.point + (hitInfo.normal * 0.01f), Quaternion.FromToRotation(Vector3.up, hitInfo.normal));
+            var d = Instantiate(decal, hitInfo.point - (hitInfo.normal * 0.001f), Quaternion.FromToRotation(Vector3.forward, hitInfo.normal));
             d.transform.parent = hitInfo.transform;
             Destroy(d, 5f);
             Destroy(gameObject);
